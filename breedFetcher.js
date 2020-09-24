8271a1a195598d74d44ccd9b2a9efdb5a297bdfe
vagrant <ttjho@hotmail.com>
2020-09-24 21:19:02 +0000

    practicing making error messages from catAPI, used JSON to get requests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,29 @@
+const input = process.argv.splice(2);
+const request = require('request');
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
+
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    if (data.message) {
+      console.log(data.message);
+
+
+    } else {
+    
+      
+      if(data.length === 0 ){
+          console.log("We couldn't find your cat")
+      } else {
+        console.log(data[0].description);
+      }
+    }
+  }
+});
+
+
+
 
